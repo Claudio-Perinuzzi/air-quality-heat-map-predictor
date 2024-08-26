e2d5bb56413b7d9c553a94aa4e1d270a07d06ed9
Claudio P <perinuzzic@gmail.com>
2024-08-25 21:16:48 -0400

    Generated seasonal maps

--- src/clean_aqi_data.py
+++ src/clean_aqi_data.py
@@ -3,6 +3,8 @@ from borough_mapping import *
 from aqi_calculations import *
 import geopandas as gpd
 import folium
+from branca.element import Template, MacroElement
+
 
 
 def filter_polutants(input_path, output_path, polutants):
@@ -77,6 +79,138 @@ def generate_annual_aqi_average_html(df, year, map_filename):
         tooltip=folium.GeoJsonTooltip(fields=['boro_cd', 'aqi'], aliases=['Borough Code:', 'AQI:'])
     ).add_to(m)
 
+    # Define legend HTML style
+    legend_html = '''
+    {% macro html(this, kwargs) %}
+
+    <div style="
+        position: fixed; 
+        bottom: 20px; 
+        left: 50%; 
+        transform: translateX(-50%);
+        width: 90%;  /* Increase width */
+        height: 60px;  /* Increase height */
+        background: linear-gradient(to right, rgb(0, 228, 0), rgb(255, 255, 0), rgb(255, 126, 0), rgb(255, 0, 0), rgb(143, 63, 151), rgb(126, 0, 35)); 
+        border:2px solid black; 
+        z-index:1000; 
+        font-size:12px;
+        line-height: 1.2em;  /* Adjust line height */
+        color: black;  /* Set font color to black */
+        text-align: center;
+        padding: 5px 10px;  /* Add padding */
+        box-sizing: border-box;
+    ">
+
+        <span style="float:left; width: 12%; text-align: center;"><strong>0-50</strong><br>(Good)</span>
+        <span style="float:left; width: 12%; text-align: center;"><strong>51-100</strong><br>(Moderate)</span>
+        <span style="float:left; width: 18%; text-align: center;"><strong>101-150</strong><br>(Unhealthy for Sensitive Groups)</span>
+        <span style="float:left; width: 14%; text-align: center;"><strong>151-200</strong><br>(Unhealthy)</span>
+        <span style="float:left; width: 14%; text-align: center;"><strong>201-300</strong><br>(Very Unhealthy)</span>
+        <span style="float:right; width: 14%; text-align: center;"><strong>301-500</strong><br>(Hazardous)</span>
+    </div>
+
+    {% endmacro %}
+    '''
+
+    # Crate a Macro Element object with the legend, and add it to the map as a child
+    legend = MacroElement()
+    legend._template = Template(legend_html)
+    m.get_root().add_child(legend)
+
     # Save the map to an HTML file with year in filename
-    map_filename = f"data/maps/Map_{year}.html"
+    map_filename = f"data/maps/annual/Map_{year}.html"
+    m.save(map_filename)
+
+
+
+#TODO: REFACTOR ABOVE & BOTTOM FUNCTIONS, SAME CODE
+def generate_seasonal_aqi_average_html(df, year, map_filename):
+    print(f"Generating {map_filename}")
+
+    # Mapping between boro_cd to color of aqi and average aqi
+    color_and_aqi = {}
+    for boro in BORO_CDS:
+        boro_cd = boro[0]  # e.g., "101"
+        districts = [boro[i] for i in range(1, len(boro))]  # List of districts
+        average_aqi = calculate_aqi_average(df, year, districts)  # Calculate AQI for current year
+        color_and_aqi[boro_cd] = [interpolate_color(average_aqi), average_aqi]
+    
+    # Load GeoJSON data
+    input_map = "data/nyc_community_districts.geojson"
+    gdf = gpd.read_file(input_map)
+    
+    # Add AQI value to GeoDataFrame properties
+    def add_aqi_to_properties(row):
+        boro_cd = row['boro_cd']
+        if boro_cd in color_and_aqi:
+            row['aqi'] = color_and_aqi[boro_cd][1]  # Add AQI value to 'aqi' property
+        else:
+            row['aqi'] = 'N/A'
+        return row
+
+    gdf = gdf.apply(add_aqi_to_properties, axis=1)
+
+    # Initialize the map
+    m = folium.Map(location=[40.7128, -74.0060], zoom_start=10)
+    
+    # Function to style features based on borough code
+    def style_function(feature):
+        boro_cd = feature['properties'].get('boro_cd')
+        color = color_and_aqi.get(boro_cd, ['gray', 'N/A'])
+        return {
+            'fillColor': color[0],
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.5
+        }
+    
+    # Add GeoJSON data to the map with custom styling
+    folium.GeoJson(
+        gdf,
+        style_function=style_function,
+        highlight_function=lambda highlight: {'fillColor': 'blue'},
+        overlay=True,
+        tooltip=folium.GeoJsonTooltip(fields=['boro_cd', 'aqi'], aliases=['Borough Code:', 'AQI:'])
+    ).add_to(m)
+
+    # Define legend HTML style
+    legend_html = '''
+    {% macro html(this, kwargs) %}
+
+    <div style="
+        position: fixed; 
+        bottom: 20px; 
+        left: 50%; 
+        transform: translateX(-50%);
+        width: 90%;  /* Increase width */
+        height: 60px;  /* Increase height */
+        background: linear-gradient(to right, rgb(0, 228, 0), rgb(255, 255, 0), rgb(255, 126, 0), rgb(255, 0, 0), rgb(143, 63, 151), rgb(126, 0, 35)); 
+        border:2px solid black; 
+        z-index:1000; 
+        font-size:12px;
+        line-height: 1.2em;  /* Adjust line height */
+        color: black;  /* Set font color to black */
+        text-align: center;
+        padding: 5px 10px;  /* Add padding */
+        box-sizing: border-box;
+    ">
+
+        <span style="float:left; width: 12%; text-align: center;"><strong>0-50</strong><br>(Good)</span>
+        <span style="float:left; width: 12%; text-align: center;"><strong>51-100</strong><br>(Moderate)</span>
+        <span style="float:left; width: 18%; text-align: center;"><strong>101-150</strong><br>(Unhealthy for Sensitive Groups)</span>
+        <span style="float:left; width: 14%; text-align: center;"><strong>151-200</strong><br>(Unhealthy)</span>
+        <span style="float:left; width: 14%; text-align: center;"><strong>201-300</strong><br>(Very Unhealthy)</span>
+        <span style="float:right; width: 14%; text-align: center;"><strong>301-500</strong><br>(Hazardous)</span>
+    </div>
+
+    {% endmacro %}
+    '''
+
+    # Crate a Macro Element object with the legend, and add it to the map as a child
+    legend = MacroElement()
+    legend._template = Template(legend_html)
+    m.get_root().add_child(legend)
+
+    # Save the map to an HTML file with year in filename
+    map_filename = f"data/maps/seasonal/Map_{year}.html"
     m.save(map_filename)
--- src/main.py
+++ src/main.py
@@ -7,8 +7,12 @@ from clean_aqi_data import *
 from aqi_calculations import *
 from borough_mapping import *
 
-from io import BytesIO
-import base64
+# TODO:
+# Implement a option for a button to change the color for a more drastic appearence, 
+# the highest mean in the data set will be the end color
+# This will be more apparent for the predictive model
+    # possibly use a choropleth map where different ranges of aqi are rep with diff colors
+    # may help in distinguishing beteween various ranges more clearly
 
 
 def main():
@@ -26,81 +30,77 @@ def main():
     df = pd.read_csv(output_path)
     append_aqi_to_df(df)
 
+    #TODO Put in respective tabs & refactor ######################################
+
     # Generate HTMLs for the annual AQI averages if they don't exist
     for year in ANNUAL_AQI_AVERAGE:
-        map_filename = f"data/maps/Map_{year}.html"
+        map_filename = f"data/maps/annual/Map_{year}.html"
         if not os.path.exists(map_filename):
             generate_annual_aqi_average_html(df, year, map_filename)
         else:
             print(f"{map_filename} exists")
 
 
+    # Generate HTMLs for seasonal AQI averages if they don't exisit
+    for season in SEASONAL_AQI_AVERAGE:
+        map_filename = f"data/maps/seasonal/Map_{season}.html"
+        if not os.path.exists(map_filename):
+            generate_seasonal_aqi_average_html(df, season, map_filename)
+        else:
+            print(f"{map_filename} exists")
+
+
+    # TODO: Refactor Tabs
+    # Render HTML to Streamlit ########################################
+    # Create Tabs
+    tab1, tab2 = st.tabs(["Annual AQI Average", "Seasonal AQI Averwge"])
+
+    # Annual AQI Average tab
+    with tab1:
+        st.title("NYC Annual Average AQI Heatmap")
+
+        # Render slidebar and map slidebar value index to annual aqi average tuple
+        year_index = st.slider("Select Year", min_value=2009, max_value=2022)
+        selected_year = ANNUAL_AQI_AVERAGE[year_index - 2009]
+
+        # Display the title of the given map
+        st.markdown(f"""
+            <h1 style="text-align: center; font-size: 28px;">
+                AQI {selected_year}
+            </h1>
+        """, unsafe_allow_html=True)
+
+        # Generate map name based on user's choice and render the HTML to streamlit
+        map_name = f"data/maps/annual/Map_{selected_year}.html"
+        with open(map_name, "r") as file:
+            html_content = file.read()
+        st.components.v1.html(html_content, height=600)
+    
+    with tab2:
+        st.title("NYC Seasonal Average AQI Heatmap")
+
+        # Render slidebar and map slidebar value index to seasonal aqi average tuple
+        season_index = st.slider("Select Season", min_value=0, max_value=len(SEASONAL_AQI_AVERAGE) - 1)
+        selected_season = SEASONAL_AQI_AVERAGE[season_index]
+
+        # Display the title of the given map
+        st.markdown(f"""
+            <h1 style="text-align: center; font-size: 28px;">
+                AQI {selected_season}
+            </h1>
+        """, unsafe_allow_html=True)
+
+        # Generate map name based on user's choice and render the HTML to streamlit
+        map_name = f"data/maps/seasonal/Map_{selected_season}.html"
+        with open(map_name, "r") as file:
+            html_content = file.read()
+        st.components.v1.html(html_content, height=600)
+
+    
+ 
+
     
 
-    
-    # TODO: ADD STREAMLIT TO RENDER HTML FROM DATA/MAPS DIRECTORY
-
-    # st.title('NYC AQI Heatmap by Year')
-
-    # # Slider for selecting year
-    # year_index = st.slider('Select Year', min_value=0, max_value=len(ANNUAL_AQI_AVERAGE) - 1)
-
-    # # Get selected year
-    # selected_year = ANNUAL_AQI_AVERAGE[year_index]
-
-    # # Load GeoJSON data
-    # input_map = "data/nyc_community_districts.geojson"
-    # gdf = gpd.read_file(input_map)
-
-    # # Calculate AQI and color mapping for the selected year
-    # color_and_aqi = {}
-    # for boro in BORO_CDS:
-    #     boro_cd = boro[0]
-    #     districts = [boro[i] for i in range(1, len(boro))]
-    #     average_aqi = calculate_aqi_average(df, selected_year, districts)
-    #     color_and_aqi[boro_cd] = [interpolate_color(average_aqi), average_aqi]
-
-    # # Add AQI value to GeoDataFrame properties
-    # def add_aqi_to_properties(row):
-    #     boro_cd = row['boro_cd']
-    #     if boro_cd in color_and_aqi:
-    #         row['aqi'] = color_and_aqi[boro_cd][1]
-    #     else:
-    #         row['aqi'] = 'N/A'
-    #     return row
-
-    # gdf = gdf.apply(add_aqi_to_properties, axis=1)
-
-    # # Initialize the map
-    # m = folium.Map(location=[40.7128, -74.0060], zoom_start=10)
-
-    # # Function to style features based on borough code
-    # def style_function(feature):
-    #     boro_cd = feature['properties'].get('boro_cd')
-    #     color = color_and_aqi.get(boro_cd, ['gray', 'N/A'])
-    #     return {
-    #         'fillColor': color[0],
-    #         'color': 'black',
-    #         'weight': 1,
-    #         'fillOpacity': 0.5
-    #     }
-
-    # # Add GeoJSON data to the map with custom styling
-    # folium.GeoJson(
-    #     gdf,
-    #     style_function=style_function,
-    #     highlight_function=lambda highlight: {'fillColor': 'blue'},
-    #     overlay=True,
-    #     tooltip=folium.GeoJsonTooltip(fields=['boro_cd', 'aqi'], aliases=['Borough Code:', 'AQI:'])
-    # ).add_to(m)
-
-    # # Save map to a BytesIO object and display in Streamlit
-    # map_html = BytesIO()
-    # m.save(map_html, close_file=False)
-    # map_html.seek(0)
-
-    # # Display the map in Streamlit
-    # st.components.v1.html(map_html.getvalue().decode(), height=600)
     
 
 
